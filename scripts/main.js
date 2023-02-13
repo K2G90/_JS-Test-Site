4634d7c5101d1f9ca98addad5e097864487b3636
Cedric L. Williams Jr <clw.jr90@gmail.com>
2023-02-13 15:38:06 -0600

    added image flip behavior

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,9 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const myImage = document.querySelector("img");
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/black-panther-wakanda-king-2020-0c-2880x1800.jpeg") {
+      myImage.setAttribute("src", "images/black-panther-4k-minimalism-2020-gd-2880x1800.jpeg");
+    } else {
+      myImage.setAttribute("src", "images/black-panther-wakanda-king-2020-0c-2880x1800.jpeg");
+    }
+  };
